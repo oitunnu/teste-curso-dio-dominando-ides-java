5bba5bfe47b74a52822e6356c71648484a3b8012
Itunnu <itunnu.yussuf@gmail.com>
2023-01-14 09:23:08 -0300

    add documentation

--- src/com/dio/calculadora/Calculadora.java
+++ src/com/dio/calculadora/Calculadora.java
@@ -1,6 +1,15 @@
 package com.dio.calculadora;
-
 import java.util.Scanner;
+
+/**
+ * <h1>Calculator</h1>
+ * This calculator has 4 different functions:
+ * add, subtract, divide and multiply
+ * It accepts integer as well as float
+ * 
+ * @author Omotayo Itunnu Yussuf
+ * @version 1.0
+ */
 
 public class Calculadora {
 
@@ -9,37 +18,63 @@ public class Calculadora {
 		
 		double a, b;
 		
+		//get input
 		System.out.println("Digite o primeiro valor: ");
 		a = scan.nextInt();
 		System.out.println("Digite o segundo valor: ");
 		b = scan.nextInt();
 		
-		double adicao = adicao(a, b);
-		double subtracao = subtracao(a, b);
-		double divisao = divisao(a, b);
-		double multiplicacao = multiplicacao(a, b);
+		//the operations
+		double addition = add(a, b);
+		double subtraction = subtract(a, b);
+		double division = divide(a, b);
+		double multiplication = multiply(a, b);
 		
-		System.out.println("adicao " + adicao);
-		System.out.println("subtracao " + subtracao);
-		System.out.println("divisao " + divisao);
-		System.out.println("multiplicacao " + multiplicacao);
+		//print results
+		System.out.println("adicao " + addition);
+		System.out.println("subtracao " + subtraction);
+		System.out.println("divisao " + division);
+		System.out.println("multiplicacao " + multiplication);
 		scan.close();
 		
 	}
-
-	public static double adicao(double a, double b) {
+/**
+ * This method adds two numbers
+ * @param a the first number
+ * @param b the second number
+ * @return the addition of the two numbers given
+ */
+	public static double add(double a, double b) {
 		return a + b;
 	}
 	
-	public static double subtracao(double a, double b) {
+/**
+ * This method subtracts two numbers
+ * @param a the first number
+ * @param b the second number
+ * @return the result of the subtraction of (a - b)
+ */
+	public static double subtract(double a, double b) {
 		return a - b;
 	}
-	
-	public static double divisao(double a, double b) {
+
+/**
+ * This method divide one number by another
+ * @param a the first number
+ * @param b the second number
+ * @return the division of the two numbers given as in (a/b)
+ */
+	public static double divide(double a, double b) {
 		return a / b;
 	}
-	
-	public static double multiplicacao(double a, double b) {
+
+/**
+ * This method multiply two numbers
+ * @param a the first number
+ * @param b the second number
+ * @return the result of the multiplication of the two numbers given
+ */
+	public static double multiply(double a, double b) {
 		return a * b;
 	}
 }
